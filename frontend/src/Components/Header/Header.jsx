a84182b891567e2442cb9caa215f437b7222d2a3
Modolan1 <modupeogunmuyiwa85@gmail.com>
2026-06-09 23:05:32 -0500

    wrong spelligs corrections

--- frontend/src/Components/Header/Header.jsx
+++ frontend/src/Components/Header/Header.jsx
@@ -5,8 +5,8 @@ const Header = ({ onViewMenuClick }) => {
   return (
     <div className='header'>
         <div className='header-contents'>
-    <h1 >Africa food at its best! </h1>
-            <p>Order your  sumcious Africa dishes,
+    <h1 >African food at its best! </h1>
+            <p>Order your  sumptuous African dishes,
         made with the freshest ingredients and traditional recipes. </p>
         <button onClick={onViewMenuClick}>View Menu </button>
 
